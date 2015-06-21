0928fdb0ec96caf71572fa8bedfa3f2a9ceaf921
Gaetan Pueo <gaetanpueo@Gaetans-MacBook-Pro.local>
2015-06-21 13:53:11 +0200

    account

---
+++ htdocs/views/account.v.php
@@ -0,0 +1,39 @@
+<style>
+	#Account
+	{
+		
+	}
+	
+	#Module_Account,
+	#Module_Purchase,
+	#Module_Products,
+	#Module_Categories,
+	#Module_Users
+	{
+		width: 100%;
+	}
+</style>
+
+<div id="Account" class="page">
+
+	<div id="Module_Account">
+		
+	</div>
+	
+	<div id="Module_Purchases">
+		
+	</div>
+	
+	<div id="Module_Products">
+		
+	</div>
+	
+	<div id="Module_Categories">
+		
+	</div>
+	
+	<div id="Module_Users">
+		
+	</div>
+
+</div>
